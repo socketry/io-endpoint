8795a000a2971fc41d17ce50a717af6120e08006
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-04-21 21:47:40 +1200

    Improved shims for `IO#readable?`.
    It turns out calling `eof?` while other reads are in progress is likely to
    cause data corruption. It's entirely possible for the connection pool to
    invoke this code path, so we can't use `eof?`.

--- lib/io/readable.rb
+++ lib/io/readable.rb
@@ -1,12 +1,8 @@
 class IO
 	unless method_defined?(:readable?)
 		def readable?
-			if self.wait_readable(0).nil?
-				# timeout means it is not eof
-				return true
-			else
-				!self.eof?
-			end
+			# Do not call `eof?` here as it is not concurrency-safe and it can block.
+			!closed?
 		end
 	end
 end
@@ -16,19 +12,15 @@ require 'socket'
 class BasicSocket
 	unless method_defined?(:readable?)
 		def readable?
-			# Is it likely that the socket is still connected?
-			# May return false positive, but won't return false negative.
-			return false unless super
-			
 			# If we can wait for the socket to become readable, we know that the socket may still be open.
-			result = to_io.recv_nonblock(1, MSG_PEEK, exception: false)
+			result = self.recv_nonblock(1, MSG_PEEK, exception: false)
 			
 			# No data was available - newer Ruby can return nil instead of empty string:
 			return false if result.nil?
 			
 			# Either there was some data available, or we can wait to see if there is data avaialble.
 			return !result.empty? || result == :wait_readable
-		rescue Errno::ECONNRESET
+		rescue Errno::ECONNRESET, IOError
 			# This might be thrown by recv_nonblock.
 			return false
 		end
