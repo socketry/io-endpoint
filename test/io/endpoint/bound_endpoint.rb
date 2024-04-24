bc9d209cf0d7ae7dc1052fbcff3b49e781300ff0
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-04-24 13:37:44 +1200

    Modernize gem.

--- test/io/endpoint/bound_endpoint.rb
+++ test/io/endpoint/bound_endpoint.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2023, by Samuel Williams.
+# Copyright, 2023-2024, by Samuel Williams.
 
 require 'io/endpoint/bound_endpoint'
 require 'io/endpoint/connected_endpoint'
