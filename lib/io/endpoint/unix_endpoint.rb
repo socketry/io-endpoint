308c97b6148b3efe6ac5e744992a3e7202aee694
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-02-20 20:01:39 +1300

    Pass all arguments to `UNIXEndpoint#bind` through to super.

--- lib/io/endpoint/unix_endpoint.rb
+++ lib/io/endpoint/unix_endpoint.rb
@@ -35,7 +35,7 @@ module IO::Endpoint
 			return false
 		end
 		
-		def bind(&block)
+		def bind(...)
 			super
 		rescue Errno::EADDRINUSE
 			# If you encounter EADDRINUSE from `bind()`, you can check if the socket is actually accepting connections by attempting to `connect()` to it. If the socket is still bound by an active process, the connection will succeed. Otherwise, it should be safe to `unlink()` the path and try again.
