69217f434fa53a694cacadd96e9b218c0d241df2
Delton Ding <dsh0416@gmail.com>
2025-05-27 17:51:50 +0800

    Support unix path longer than 104 bytes

--- lib/io/endpoint/unix_endpoint.rb
+++ lib/io/endpoint/unix_endpoint.rb
@@ -9,8 +9,15 @@ module IO::Endpoint
 	# This class doesn't exert ownership over the specified unix socket and ensures exclusive access by using `flock` where possible.
 	class UNIXEndpoint < AddressEndpoint
 		def initialize(path, type = Socket::SOCK_STREAM, **options)
-			# I wonder if we should implement chdir behaviour in here if path is longer than 104 characters.
-			super(Address.unix(path, type), **options)
+			# If the path is longer than 104 characters, we need to change directory to the parent directory of the socket.
+			if path.bytesize <= 104
+				super(Address.unix(path, type), **options)
+			else
+				path = File.expand_path(path) # make sure the path is absolute
+				Dir.chdir(File.dirname(path)) do
+					super(Address.unix(File.basename(path), type), **options)
+				end
+			end
 			
 			@path = path
 		end
